9af784a630dd744de2ff12581f10f3dc9186d62d
samdng <samdanoga@gmail.com>
2024-05-04 20:49:27 -0500

    SI

--- Taller 4/script.js
+++ Taller 4/script.js
@@ -10,8 +10,8 @@ function validar() {
     const nombre = document.getElementById('nombre').value;
     const apellido = document.getElementById('apellido').value;
     const direccion = document.getElementById('direccion').value;
-    const correo = document.getElementById('correo').value;
-    const cpwd = document.getElementById('cpwd').value;
+    const ccpaswd = document.getElementById('ccpaswd').value;
+    const ccusuario = document.getElementById('ccusuario').value;
     const email = document.getElementById('email').value;
     const telefono = document.getElementById('telefono').value;
   
@@ -22,37 +22,36 @@ function validar() {
     }
 
     // Validar id_pais
-    if (id_pais.length>3) {
+    if (id_pais.length>3  || !Number.isInteger(parseInt(id_usr))) {
       alert("Por favor, introduce un ID de pais válido.");
       return false;
     }
   
     // Validar nombre
-    if (!/^[a-zA-Z]+$/.test(nombre)) {
+    if (!/^[a-zA-Z]+$/.test(nombre) || nombre.length>25) {
       alert("Por favor, introduce un nombre válido.");
       return false;
     }
   
     // Validar apellido
-    if (!/^[a-zA-Z]+$/.test(apellido)) {
+    if (!/^[a-zA-Z]+$/.test(apellido) || apellido.length>25) {
       alert("Por favor, introduce un apellido válido.");
       return false;
     }
   
     // Validar dirección
-    if (direccion === "") {
-      alert("Por favor, introduce una dirección.");
+    if (!/^[a-zA-Z0-9\s,.-]+$/.test(direccion)|| direccion.length>45) {
+      alert("Por favor, introduce una dirección válida.");
       return false;
     }
   
-    // Validar correo
-    if (!/^[\w-]+(\.[\w-]+)*@([\w-]+\.)+[a-zA-Z]{2,7}$/.test(correo)) {
-      alert("Por favor, introduce un correo válido.");
+    if (ccusuario.length>20) {
+      alert("Por favor, introduzca un usuario válido.");
       return false;
     }
-  
+
     // Validar cpwd
-    if (cpwd.length < 8) {
+    if (ccpaswd.length < 8 ||) {
       alert("La contraseña debe tener al menos 8 caracteres.");
       return false;
     }
