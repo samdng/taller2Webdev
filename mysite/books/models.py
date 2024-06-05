1efb65ee0d52c989e132721fc74b8e3a20d87083
samdng <samdanoga@gmail.com>
2024-06-04 20:07:45 -0500

    Update models.py

--- mysite/books/models.py
+++ mysite/books/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+class Author(models.Model):
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    country = models.CharField(max_length=30)
+
+class Category(models.Model):
+    author = models.CharField(max_length=30)
+
+class Books(models.Model):
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    title = models.CharField(max_length=30)
+    description = models.CharField(max_length=500)
