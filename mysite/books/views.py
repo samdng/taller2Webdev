701d8b72ccff04f16e1a41c64a18354975a1d961
samdng <samdanoga@gmail.com>
2024-06-05 20:50:55 -0500

    INDEX y base

--- mysite/books/views.py
+++ mysite/books/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
+
+from .models import Author
 # Create your views here.
 
 def index(request):
+    autores = Author.objects.all()
     template = loader.get_template("books/index.html")
-    context = {}
-    return HttpResponse("Hello World.")
+    context = {
+        "autores":autores,
+    }
+    return HttpResponse(template.render(context,request))
