b905ceb56cc7dd1814588b8319960ff37042b1f0
samdng <samdanoga@gmail.com>
2024-06-13 16:27:18 -0500

    base e index con el extra head

--- mysite/books/views.py
+++ mysite/books/views.py
@@ -4,11 +4,15 @@ from django.template import loader
 
 from .models import Author
 # Create your views here.
-
 def index(request):
     autores = Author.objects.all()
     template = loader.get_template("books/index.html")
     context = {
         "autores":autores,
     }
+    return HttpResponse(template.render(context,request))
+
+def home(request):
+    template = loader.get_template("index.html")
+    context = {}
     return HttpResponse(template.render(context,request))
