c6cb4850852a7e2dd45ba5c1e15a795e0b1cf08d
samdng <samdanoga@gmail.com>
2024-06-15 22:10:54 -0500

    forms

--- mysite/books/views.py
+++ mysite/books/views.py
@@ -22,3 +22,20 @@ def authors(request):
     template = loader.get_template('books/authors.html')
     context = {'autores':autores,}
     return HttpResponse(template.render(context,request))
+
+def new_author(request):
+    if request.method == 'POST':
+        form = AuthorForm(request.POST)
+
+        if form.is_valid():
+            nombre = form.cleaned_data['first_name']
+            apellido = form.cleaned_data['last_name']
+            pais  = form.cleaned_data['country']
+            author = Author(first_name=nombre,last_name=apellido,country=pais)
+            author.save()
+            return HttpResponseRedirect(reverse('authors'))
+    
+    else:
+        form = AuthorForm()
+    return render(request, 'books/create_authors.html',{'form':form})
+
