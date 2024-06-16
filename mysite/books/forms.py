96cc1a4e11d7dd060b3a6ddce7f9e350effbda87
samdng <samdanoga@gmail.com>
2024-06-15 21:56:08 -0500

    forms.py

---
+++ mysite/books/forms.py
@@ -0,0 +1,24 @@
+from django import forms
+
+from .models import Author
+
+class AuthorForm(forms.Mode1Form):
+    class Meta:
+        model = Author
+
+        fields = [
+            'first_name',
+            'last_name',
+            'country',
+        ]
+        
+        labels = {
+            'first_name' : 'Nombre',
+            'last_name' : 'Apellidos',
+            'country' : 'Pais',
+        }
+        widgets = {
+            'first_name': forms.TextInput(attrs={'class':'form-control','required':''})
+            'last_name': forms.TextInput(attrs={'class':'form-control','required':''})
+            'country': forms.TextInput(attrs={'class':'form-control','required':''})
+        }
