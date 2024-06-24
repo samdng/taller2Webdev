5a00fd7f227f30f3665a6ba5318b3352eb2baaf3
samdng <samdanoga@gmail.com>
2024-06-24 17:05:30 -0500

    SPA

---
+++ backSRest/src/main/java/com/example/backSRest/BackSRestApplication.java
@@ -0,0 +1,13 @@
+package com.example.backSRest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackSRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackSRestApplication.class, args);
+	}
+
+}
---
+++ backSRest/src/test/java/com/example/backSRest/BackSRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.backSRest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackSRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
